305449ed00c1b3eaf7572a1d01eee1c0e0ea46ee
Sumon Roy <162395226+sumonstr12@users.noreply.github.com>
2024-05-28 04:38:25 +0600

    simple code

---
+++ Source_Code/javaScript/script.js
@@ -0,0 +1,66 @@
+// initialising a variable name datap
+// initialising a variable name datab
+
+let datap = 299;    // inital value for pro plan
+let datab = 199;    // inital value for basic plan
+
+//printing default value of pro-plan or basic-plan
+document.getElementById("countingp").innerText = datap;
+document.getElementById("countingb").innerText = datab;
+
+//creation of increment function for pro-plan
+let nump = 1;   //assign value for selecting room number
+document.getElementById("roomp").innerText = nump;
+function incrementp() {
+	datap = datap *2;
+    nump = nump +1;
+	document.getElementById("countingp").innerText = datap;
+    document.getElementById("roomp").innerText = nump;
+}
+//creation of decrement function for pro-plan
+function decrementp() {
+	if(datap/2>=299){
+        datap = datap /2;
+        nump = nump -1;
+        document.getElementById("countingp").innerText = datap;
+        document.getElementById("roomp").innerText = nump;
+    }
+}
+
+let numb = 1;   //assign value for selecting room number
+document.getElementById("roomb").innerText = numb;
+
+//creation of increment function for basic-plan
+function incrementb() {
+    datab = datab *2;
+    numb = numb +1;
+    document.getElementById("countingb").innerText = datab;
+    document.getElementById("roomb").innerText = numb;
+}
+//creation of decrement function for basic-plan
+function decrementb() {
+    if(datab/2>=199){
+        datab = datab/2;
+        document.getElementById("countingb").innerText = datab;
+        numb = numb -1;
+        document.getElementById("roomb").innerText = numb;
+    }
+}
+
+
+// Message handlers after signing up.
+// for basic-plan
+const smsb = document.getElementById('messageb');
+const successMessageb = document.getElementById('success-messageb');
+smsb.addEventListener('submit', (e) => {
+    successMessageb.textContent = `Thank you for choosing ${numb} room`;
+});
+
+// for pro-plan
+const smsp = document.getElementById('messagep');
+const successMessagep = document.getElementById('success-messagep');
+smsp.addEventListener('submit', (e) => {
+    successMessagep.textContent = `Thank you for choosing ${nump} room`;
+});
+
+
---
+++ Source_Code/readMe.md
@@ -0,0 +1,38 @@
+# Assignment: Interior WebDesign
+
+In this assignment, i created a website according to the mentioned task.
+
+## Working Process
+
+```python
+in this assignment,
+i am followed some steps.
+
+#step-01: divided body into two parts.(container and document).
+#step-02: divided container part into sub-div and document portion is 6 section.
+#step-03: each parts contains their's properties according to the pdf.
+#step-04: linked #CSS file and #Js file with the #HTML file.
+
+'Happy Learning'
+```
+
+# Getting started
+
+`index.html` is the main file html file.
+opening this file the website launch automatically.
+
+## About me :
+
+```
+Name : Sumon Roy
+```
+### educational qualification :
+```
+Computer Science and Engineering Discipline,
+Khulna University.
+Khulna.
+```
+### Social Link :
+
+1. `Facebook` [Sumon Roy](https://www.facebook.com/sumonroysnr/)
+2. `Linkedin` [Sumon Chandra Barman](https://www.linkedin.com/in/sumon-str/)
